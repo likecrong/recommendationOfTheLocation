3ca02cc13c870dec4939f58777c2cbf8d2aee9fb
likecrong <112467598+likecrong@users.noreply.github.com>
2022-11-08 15:21:29 +0900

    Delete 06_R회귀분석.r

--- 06_R회귀분석.r
+++
@@ -1,7 +0,0 @@
-#R 회귀분석 실행코드
-dat = read.csv(file='./data_clean.csv')
-reg<-lm(a_priceOfQ ~ bus_dis + sub_dis + pa_dis + ve_dis + a_numOfQua 
-      + a_m + a_f + a_a20 + a_a30 + a_a40 + a_a50, data=df)
-summary(reg)
-par(a)
-plot(a)
